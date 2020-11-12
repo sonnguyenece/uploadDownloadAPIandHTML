b1d14efe72fb6958c2211bd6c914d15454db858a
nmson <nmson@ecpay.vn>
2020-11-12 16:31:02 +0700

    api upload and download files

--- UploaddownloadfileApplication.java
+++ UploaddownloadfileApplication.java
@@ -1,9 +1,14 @@
 package com.example.uploaddownloadfile;
 
+import com.example.uploaddownloadfile.property.FileStorageProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
+@EnableConfigurationProperties({
+        FileStorageProperties.class
+})
 public class UploaddownloadfileApplication {
 
     public static void main(String[] args) {
