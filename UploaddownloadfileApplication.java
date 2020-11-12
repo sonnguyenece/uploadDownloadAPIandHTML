2c62e991f6a0fc09dd8646efdc3b02ad907a0444
nmson <nmson@ecpay.vn>
2020-11-12 09:41:33 +0700

    git init

---
+++ UploaddownloadfileApplication.java
@@ -0,0 +1,13 @@
+package com.example.uploaddownloadfile;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UploaddownloadfileApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UploaddownloadfileApplication.class, args);
+    }
+
+}
